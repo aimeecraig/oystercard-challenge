ccda10494665a77e3e3990b72a37b7612df0a258
Aimée Craig <aimeecraig1@gmail.com>
2018-10-08 20:30:09 +0100

    code updated to pass exceeding balance unit test

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,18 @@
 class Oystercard
   attr_accessor :balance
 
+  MAX_BALANCE = 90
+
   def initialize
     @balance = 0
   end
 
   def top_up(amount)
     @balance += amount
-    return @balance
+    if @balance > MAX_BALANCE
+      raise 'Card has exceeded £90 limit.'
+    else
+      return @balance
+    end
   end
 end

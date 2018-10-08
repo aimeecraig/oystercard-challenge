feb1b0cc9dfae2ddadcb4de18b1ffe5f9852a4ae
Aimée Craig <aimeecraig1@gmail.com>
2018-10-08 17:22:22 +0100

    code amended to pass top up unit test

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,7 +1,12 @@
 class Oystercard
-  attr_reader :balance
+  attr_accessor :balance
 
   def initialize
     @balance = 0
+  end
+
+  def top_up(amount)
+    @balance += amount
+    return @balance
   end
 end

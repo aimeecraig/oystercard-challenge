501b431e553d4e35866e8f4da5c6a32393292dd8
Aimée Craig <aimeecraig1@gmail.com>
2018-10-08 17:11:32 +0100

    unit test written to test top up function

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,8 @@ describe Oystercard do
   it 'creates a new card with a default balance of 0' do
     expect(subject.balance).to eq 0
   end
+
+  it 'tops up a new card with 10' do
+    expect(subject.top_up(10)).to eq 10
+  end
 end
